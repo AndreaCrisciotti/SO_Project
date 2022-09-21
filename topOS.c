26a5bd82c11195456ee6ee2aa7b207504e7be121
Andrea Crisciotti <crisciotti.1689186@studenti.uniroma1.it>
2022-09-21 19:35:36 +0200

    Modify topOS.c for Mount PROC #1

--- topOS.c
+++ topOS.c
@@ -1,70 +1,54 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mount.h>
 #include <string.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
 #include "binary_file_search.h"
 
-typedef struct {
-  char family_name[128];
-  char name[128];
-} SampleStruct;
+typedef struct{
+    char pid[128];
+    char user[128];
+    char pr[128];
+    char ni[128];
+    char virt[128];
+    char name[128];
+}TopStruct;
 
-void SampleStruct_init(SampleStruct* dest,
-		       char* family_name,
-		       char* name){
-  strcpy(dest->family_name, family_name);
-  strcpy(dest->name, name);
+
+void TopStruct_init(TopStruct* top, char* pid,char* user, char* pr, char* ni, char* virt, char* name){
+  strcpy(top->pid, pid);
+  strcpy(top->user, user);
+  strcpy(top->pr, pr);
+  strcpy(top->ni, ni);
+  strcpy(top->virt, virt);
+  strcpy(top->name, name);
 }
-
-int SampleStruct_compare(void* a, void* b){
-  SampleStruct* sa=(SampleStruct*) a;
-  SampleStruct* sb=(SampleStruct*) b;
-  int cmp1=strcmp(sa->family_name, sb->family_name);
-  if (cmp1)
-    return cmp1;
-  return strcmp(sa->name, sb->name);
-}
-
 
 int main(int argc, char** argv){
-  if (argc<2){
-    printf("binary_test <file>");
-  }
-  int fd=open(argv[1], O_CREAT|O_RDWR,0666);
-  SampleStruct v;
-  SampleStruct_init(&v,"AA","aa");
-  write(fd, &v, sizeof(v));
-  SampleStruct_init(&v,"CC","cc");
-  write(fd, &v, sizeof(v));
-  SampleStruct_init(&v,"DD","DD");
-  write(fd, &v, sizeof(v));
-  SampleStruct_init(&v,"EE","ee");
-  write(fd, &v, sizeof(v));
-  SampleStruct_init(&v,"KK","lklklkl");
-  write(fd, &v, sizeof(v));
-  close(fd);
-
   
+    printf("*********************SO_Project Crisciotti Andrea*********************\n");
 
-  fd=open(argv[1], O_RDONLY,0666);
-  int i=4; // read in reverse order!!
-  while(!binaryFileRead(fd, &v, sizeof(SampleStruct), i)){
-    printf("name: %s, family_name:%s\n", v.name, v.family_name);
-    --i;
-  }
-  close(fd);
+    const char* src  = "none";
+    const char* trgt = "/proc";
+    const unsigned long mntflags = 0;
 
-  fd=open(argv[1], O_RDONLY,0666);
-  SampleStruct_init(&v, "DD", "DD");
-  int pos=binaryFileSearch(fd, &v, sizeof(SampleStruct), SampleStruct_compare);
-  printf("found pos:%d\n", pos);
+    int i = mount(src,trgt,"proc",mntflags,"");
+    if (i == 0)
+    {
+        printf("\nMount created at %s...\n", trgt);
+        umount(trgt);
+    }
+    else
+    {
+        printf("\nError : Failed to mount \n"
+                "Reason: %s [%d]\n"
+                , strerror(errno), errno);
+        return -1;
+    }
 
-  SampleStruct_init(&v, "BB", "DD");
-  pos=binaryFileSearch(fd, &v, sizeof(SampleStruct), SampleStruct_compare);
-  printf("found pos:%d\n", pos);
-
-  close(fd);
-  
+    printf("%d",i);
+    umount(trgt);
 }
