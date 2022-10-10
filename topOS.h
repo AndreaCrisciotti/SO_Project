5bddd8c3b3605bb6eb4465677d913e12bd1038da
Andrea Crisciotti <38427839+AndreaCrisciotti@users.noreply.github.com>
2022-10-10 17:26:32 +0200

    Insert Text with FIGlet and TOIlet

--- topOS.h
+++ topOS.h
@@ -14,9 +14,14 @@
 
 // In this project, I have used the most type to comunicate to the Operating System  
 /*
-       
-        
-
+         _____            ___  ____    ____            _           _   
+        |_   _|__  _ __  / _ \/ ___|  |  _ \ _ __ ___ (_) ___  ___| |_ 
+          | |/ _ \| '_ \| | | \___ \  | |_) | '__/ _ \| |/ _ \/ __| __|
+          | | (_) | |_) | |_| |___) | |  __/| | | (_) | |  __/ (__| |_ 
+          |_|\___/| .__/ \___/|____/  |_|   |_|  \___// |\___|\___|\__|
+                  |_|                               |__/               
+                            
+          ******************Made by FIGlet and TOIlet******************
  */
 //Definition of struct process
 struct TopStruct{
